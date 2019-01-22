97c163266a11e3f3860a5e190abf84d79506a677
Andriy Stefanchuk <>
2019-01-22 09:19:38 +0200

    Added throttle demo.

---
+++ SwiftFlickrSearch-master/FlickrSearch/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  FlickrSearch
+//
+//  Created by Kyle Clegg on 12/09/14.
+//  Copyright (c) 2014 Kyle Clegg. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
